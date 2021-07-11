f1e3a38fb3659f26d6627582cb5451618d50b64c
Nikita Kovalevskiy <koval701@gmail.com>
2021-07-11 14:58:05 +0700

    done

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -1,22 +1,8 @@
 from django import forms
-from .models import Group, Post
-from django.contrib.auth import get_user_model
-
-
-User = get_user_model()
+from .models import Post
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["text", "group"]
-        # text = forms.CharField(widget=forms.Textarea)
-        # group = forms.ModelChoiceField(queryset=Group.objects.all(), required=False)
-        
-
-class NewPostForm(PostForm):
-    pass
-
-class EditPostForm(forms.ModelForm):
-    text = forms.CharField(widget=forms.Textarea)
-    group = forms.ModelChoiceField(queryset=Group.objects.all(), required=False)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -16,24 +16,15 @@ def index(request):
                   )
 
 
-# view-функция для страницы сообщества
 def group_posts(request, slug):
-    """
-    функция get_object_or_404 получает по заданным критериям объект из базы
-    данных или возвращает сообщение об ошибке, если объект не найден
-    """
     group = get_object_or_404(Group, slug=slug)
     post_list = group.posts.all().order_by('-pub_date')
     paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
-    # Метод .filter позволяет ограничить поиск по критериям.
-    # Это аналог добавления условия WHERE group_id = {group_id}
-    # posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
     return render(request, "group.html", {"group": group, "page": page})
 
 
-# view-функция со списком сообществ
 @login_required
 def group_list(request):
     groups = Group.objects.all()
@@ -43,14 +34,9 @@ def group_list(request):
 def profile(request, username):
     user = get_object_or_404(User, username=username)
     post_list = user.posts.all()
-
     paginator = Paginator(post_list, 10)
     post_count = paginator.count
-
-    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
     page_number = request.GET.get('page')
-
-    # Получаем набор записей для страницы с запрошенным номером
     page = paginator.get_page(page_number)
     content = {
         "user_profile": user,
@@ -113,14 +99,6 @@ def post_edit(request, username, post_id):
             return redirect(f"/{username}/{post_id}")
         else:
             print(form.errors)
-        # если условие if form.is_valid() ложно и данные не прошли валидацию -
-        # передадим полученный объект в шаблон
-        # чтобы показать пользователю информацию об ошибке
-        # заодно заполним прошедшими валидацию данными все поля,
-        # чтобы не заставлять пользователя вносить их повторно
         return render(request, "post_edit.html", {"form": form})
-
-    # если пришёл не POST-запрос - создаём и передаём в шаблон пустую форму
-    # пусть пользователь напишет что-нибудь
     form = PostForm(instance=post)
     return render(request, "post_edit.html", {"form": form})
